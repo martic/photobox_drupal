dc26bf9b06392f7acf08c05dbe6f67292f5c587e
Martin Camp <martin@idlprojections.com>
2014-12-03 11:15:33 +1100

    changed to reference jquery.photobox.js

--- photobox.js
+++ photobox.js
@@ -3,9 +3,14 @@
 (function ($) {
   "use strict";
 
-  Drupal.behaviors.photobox = {};
-  Drupal.behaviors.photobox.attach = function(context, settings) {
-    $('a.photobox', context).parent().photobox('a');
+  Drupal.behaviors.photobox = {
+    attach: function(context, settings) {
+      //$('a.photobox', context).parent().photobox('a'{ time:3000,thumbs:true,autoplay:false,counter:true,loop:true,zoomable:true }, callback);
+      $('div .field-name-field-portfolio-images').photobox('a', { time:3000,thumbs:true,autoplay:false,counter:true,loop:true,zoomable:true }, callback);
+      function callback(){
+        console.log('callback for loaded content:', this);
+      };
+    }
   };
 
 })(jQuery);
